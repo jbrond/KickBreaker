4fa917f38eb203a2bc2146dd2488bad10994eded
jbrond <jcb@lite.dk>
2013-05-01 15:54:45 +0200

    Initial commit

---
+++ KickBreaker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KickBreaker
+//
+//  Created by Jan Christian Brønd on 8/24/12.
+//  Copyright (c) 2012 Jan Christian Brønd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
